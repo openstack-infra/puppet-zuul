0897af255f206ad9fbe8f8e984230944f2604a0c
Glauco Oliveira <gvinici@thoughtworks.com>
2015-08-26 11:17:09 -0300

    Add acceptance tests for puppet-zuul
    Add acceptance tests for puppet-zuul module so that once the module is
    applied we check if files were created, packages were installed and
    services were started.
    I left one pending test because this module is still not idempotent.
    
    Co-Authored-By: Bruno Tavares <btavare@thoughtworks.com>
    
    Change-Id: I2bd7dbc29c05f9e29127d48a284913bd34fae438

---
+++ spec/acceptance/packages_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper_acceptance'
+
+describe 'operating system packages' do
+  packages = [
+    package('git'),
+    package('gcc'),
+    package('python-lxml'),
+    package('python-yaml'),
+    package('python-paramiko'),
+    package('python-daemon'),
+    package('yui-compressor'),
+    package('python-paste'),
+    package('python-webob')
+  ]
+  packages << package('apache2') if ['ubuntu', 'debian'].include?(os[:family])
+  packages << package('httpd') if ['centos', 'redhat'].include?(os[:family])
+
+  packages.each do |package|
+    describe package do
+      it { should be_installed }
+    end
+  end
+end
+
+describe 'pip packages' do
+  packages = [
+    package('yappi'),
+    package('zuul')
+  ]
+
+  packages.each do |package|
+    describe package do
+      it { should be_installed.by('pip') }
+    end
+  end
+end
