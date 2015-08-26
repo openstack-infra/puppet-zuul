c7d9cf45697e988543cec3d9da23323e9d00e70a
Glauco Oliveira <gvinici@thoughtworks.com>
2015-08-26 11:17:09 -0300

    Add acceptance tests for puppet-zuul
    Add acceptance tests for puppet-zuul module so that once the module is
    applied we check if files were created, packages were installed and
    services were started.
    I left one pending test because this module is still not idempotent.
    
    Co-Authored-By: Bruno Tavares <btavare@thoughtworks.com>
    
    Change-Id: I2bd7dbc29c05f9e29127d48a284913bd34fae438

---
+++ spec/acceptance/fixtures/default.pp
@@ -0,0 +1,14 @@
+class { '::zuul':
+  proxy_ssl_cert_file_contents => file('/etc/ssl/certs/ssl-cert-snakeoil.pem'),
+  proxy_ssl_key_file_contents  => file('/etc/ssl/private/ssl-cert-snakeoil.key'),
+  zuul_ssh_private_key         => file('/tmp/zuul-ssh-keys/ssh_rsa_key'),
+}
+
+class { '::zuul::server':
+  layout_dir => '/etc/project-config/zuul'
+}
+
+class { '::zuul::merger': }
+class { '::zuul::known_hosts':
+  known_hosts_content => 'known_hosts_content',
+}
---
+++ spec/acceptance/users_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper_acceptance'
+
+describe 'users' do
+  describe user('zuul') do
+    it { should exist }
+    it { should belong_to_group 'zuul' }
+    it { should have_home_directory '/home/zuul' }
+    it { should have_login_shell '/bin/bash' }
+  end
+end
