eaa88154527b93dc7af0b996dae0b4e75f6e0c7f
Glauco Oliveira <gvinici@thoughtworks.com>
2015-08-26 11:17:09 -0300

    Add acceptance tests for puppet-zuul
    Add acceptance tests for puppet-zuul module so that once the module is
    applied we check if files were created, packages were installed and
    services were started.
    I left one pending test because this module is still not idempotent.
    
    Co-Authored-By: Bruno Tavares <btavare@thoughtworks.com>
    
    Change-Id: I2bd7dbc29c05f9e29127d48a284913bd34fae438

---
+++ spec/acceptance/fixtures/preconditions.pp
@@ -0,0 +1,55 @@
+# Installing ssl-cert in order to get snakeoil certs
+package { 'ssl-cert':
+  ensure => present,
+}
+
+# Installing pip since zuul dependencies are managed by it
+package { 'python-setuptools':
+  ensure => present,
+} -> exec { 'install pip using easy_install':
+  command => 'easy_install -U pip',
+  path    => '/bin:/usr/bin:/usr/local/bin'
+}
+
+# Checking out openstack-infra/project-config
+define git_checkout(
+  $destination = undef
+) {
+  Exec { path => '/bin:/usr/bin' }
+  exec { "cloning repository ${name} at directory ${destination}":
+    command => "git clone ${name} ${destination}",
+    creates => $destination,
+  }
+}
+
+git_checkout { 'git://git.openstack.org/openstack-infra/project-config':
+  destination => '/etc/project-config'
+}
+
+# Creating temporary directory
+define create_temporary_directory() {
+  Exec { path => '/bin:/usr/bin' }
+  exec { "create temporary ${name} directory":
+    command => "mkdir -p ${name}",
+  }
+}
+
+# Creating ssh rsa keys
+define ssh_keygen (
+  $ssh_directory = undef
+) {
+  Exec { path => '/bin:/usr/bin' }
+
+  $ssh_key_file = "${ssh_directory}/${name}"
+
+  exec { "ssh-keygen for ${name}":
+    command => "ssh-keygen -t rsa -f ${ssh_key_file} -N ''",
+    creates => $ssh_key_file,
+  }
+}
+
+$ssh_key_directory = '/tmp/zuul-ssh-keys'
+create_temporary_directory { $ssh_key_directory: }
+ssh_keygen {'ssh_rsa_key':
+  ssh_directory => $ssh_key_directory
+}
