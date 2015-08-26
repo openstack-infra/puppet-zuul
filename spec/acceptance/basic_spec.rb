12ff1554e781083631489705decf4f24b5481b58
Glauco Oliveira <gvinici@thoughtworks.com>
2015-08-26 11:17:09 -0300

    Add acceptance tests for puppet-zuul
    Add acceptance tests for puppet-zuul module so that once the module is
    applied we check if files were created, packages were installed and
    services were started.
    I left one pending test because this module is still not idempotent.
    
    Co-Authored-By: Bruno Tavares <btavare@thoughtworks.com>
    
    Depends-On: I06616c89c28a5134a33059b660b334f9799eeecb
    Change-Id: I2bd7dbc29c05f9e29127d48a284913bd34fae438

---
+++ spec/acceptance/basic_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper_acceptance'
+
+describe 'puppet-zuul module' do
+  def pp_path
+    base_path = File.dirname(__FILE__)
+    File.join(base_path, 'fixtures')
+  end
+
+  def preconditions_puppet_module
+    module_path = File.join(pp_path, 'preconditions.pp')
+    File.read(module_path)
+  end
+
+  def post_conditions_puppet_module
+    module_path = File.join(pp_path, 'postconditions.pp')
+    File.read(module_path)
+  end
+
+  def default_puppet_module
+    module_path = File.join(pp_path, 'default.pp')
+    File.read(module_path)
+  end
+
+  before(:all) do
+    apply_manifest(preconditions_puppet_module, catch_failures: true)
+  end
+
+  it 'should work with no errors' do
+    apply_manifest(default_puppet_module, catch_failures: true)
+  end
+
+  it 'should be idempotent' do
+    apply_manifest(default_puppet_module, catch_failures: true)
+    apply_manifest(default_puppet_module, catch_changes: true)
+  end
+
+  it 'should enable zuul server and zuul merger services' do
+    apply_manifest(post_conditions_puppet_module, catch_failures: true)
+  end
+end
