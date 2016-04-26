6b8e6d47ab448d8eecac1e4fe0b7182f6389ebd9
James E. Blair <jeblair@linux.vnet.ibm.com>
2016-04-26 08:57:22 -0500

    Add support for zuul launcher
    Change-Id: I814a241e3934885dcd9020b5a73e69d1aa594b8f

---
+++ manifests/launcher.pp
@@ -0,0 +1,71 @@
+# Copyright 2012-2013 Hewlett-Packard Development Company, L.P.
+# Copyright 2014 OpenStack Foundation
+# Copyright 2016 IBM Corp.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+# == Class: zuul::launcher
+#
+class zuul::launcher (
+  $ensure = undef,
+  $manage_log_conf = true,
+) {
+  service { 'zuul-launcher':
+    ensure     => $ensure,
+    name       => 'zuul-launcher',
+    enable     => true,
+    hasrestart => true,
+    require    => File['/etc/init.d/zuul-launcher'],
+  }
+
+  cron { 'zuul_repack':
+    user        => 'zuul',
+    hour        => '4',
+    minute      => '7',
+    command     => 'find /var/lib/zuul/git/ -maxdepth 3 -type d -name ".git" -exec git --git-dir="{}" pack-refs --all \;',
+    environment => 'PATH=/usr/bin:/bin:/usr/sbin:/sbin',
+    require     => [User['zuul'],
+                    File['/var/lib/zuul/git']],
+  }
+
+  if $manage_log_conf {
+    file { '/etc/zuul/launcher-logging.conf':
+      ensure => present,
+      source => 'puppet:///modules/zuul/launcher-logging.conf',
+    }
+  }
+
+  include ::logrotate
+  ::logrotate::file { 'launcher.log':
+    log     => '/var/log/zuul/launcher.log',
+    options => [
+      'compress',
+      'missingok',
+      'rotate 30',
+      'daily',
+      'notifempty',
+    ],
+    require => Service['zuul-launcher'],
+  }
+  ::logrotate::file { 'launcher-debug.log':
+    log     => '/var/log/zuul/launcher-debug.log',
+    options => [
+      'compress',
+      'missingok',
+      'rotate 30',
+      'daily',
+      'notifempty',
+    ],
+    require => Service['zuul-launcher'],
+  }
+}
