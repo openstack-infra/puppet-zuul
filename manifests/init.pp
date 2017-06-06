d8b2a88b1981b26772cbbb326e0f1c2d1af3b100
Paul Belanger <pabelanger@redhat.com>
2017-06-06 15:47:46 -0400

    Move webui bits into zuul::webui
    It is possible when you invoke ::zuul::init you don't want to install
    webui bits (eg: zuul-executor only).  So move web things into webui.pp
    and expose a flag to disable them.
    
    Change-Id: I92e55c89ee763dcf0b08749278d7bc8bdc9d7fec
    Signed-off-by: Paul Belanger <pabelanger@redhat.com>

--- manifests/init.pp
+++ manifests/init.pp
@@ -68,8 +68,8 @@ class zuul (
   $connections = [],
   $python_version = 2,
   $zuulv3 = false,
+  $webui = true,
 ) {
-  include ::httpd
   include ::pip
 
   if ($python_version == 3) {
@@ -257,119 +257,6 @@ class zuul (
     content => $zuul_ssh_private_key,
   }
 
-  file { '/var/lib/zuul/www':
-    ensure  => directory,
-    require => File['/var/lib/zuul'],
-  }
-
-  file { '/var/lib/zuul/www/lib':
-    ensure  => directory,
-    require => File['/var/lib/zuul/www'],
-  }
-
-  package { 'libjs-jquery':
-    ensure => present,
-  }
-
-  file { '/var/lib/zuul/www/jquery.min.js':
-    ensure => absent
-  }
-
-  file { '/var/lib/zuul/www/lib/jquery.min.js':
-    ensure  => link,
-    target  => '/usr/share/javascript/jquery/jquery.min.js',
-    require => [File['/var/lib/zuul/www/lib'],
-                Package['libjs-jquery']],
-  }
-
-  vcsrepo { '/opt/twitter-bootstrap':
-    ensure   => latest,
-    provider => git,
-    revision => 'v3.1.1',
-    source   => 'https://github.com/twbs/bootstrap.git',
-  }
-
-  file { '/var/lib/zuul/www/bootstrap':
-    ensure => absent
-  }
-
-  file { '/var/lib/zuul/www/lib/bootstrap':
-    ensure  => link,
-    target  => '/opt/twitter-bootstrap/dist',
-    require => [File['/var/lib/zuul/www/lib'],
-                Package['libjs-jquery'],
-                Vcsrepo['/opt/twitter-bootstrap']],
-  }
-
-  vcsrepo { '/opt/jquery-visibility':
-    ensure   => latest,
-    provider => git,
-    revision => 'master',
-    source   => 'https://github.com/mathiasbynens/jquery-visibility.git',
-  }
-
-  file { '/var/lib/zuul/www/jquery-visibility.min.js':
-    ensure => absent
-  }
-
-  exec { 'install-jquery-visibility':
-    command     => 'yui-compressor -o /var/lib/zuul/www/lib/jquery-visibility.js /opt/jquery-visibility/jquery-visibility.js',
-    path        => 'bin:/usr/bin',
-    refreshonly => true,
-    subscribe   => Vcsrepo['/opt/jquery-visibility'],
-    require     => [File['/var/lib/zuul/www/lib'],
-                    Package['yui-compressor'],
-                    Vcsrepo['/opt/jquery-visibility']],
-  }
-
-  vcsrepo { '/opt/graphitejs':
-    ensure   => latest,
-    provider => git,
-    revision => 'master',
-    source   => 'https://github.com/prestontimmons/graphitejs.git',
-  }
-
-  file { '/var/lib/zuul/www/jquery.graphite.js':
-    ensure => absent
-  }
-
-  file { '/var/lib/zuul/www/lib/jquery.graphite.js':
-    ensure  => link,
-    target  => '/opt/graphitejs/jquery.graphite.js',
-    require => [File['/var/lib/zuul/www/lib'],
-                Vcsrepo['/opt/graphitejs']],
-  }
-
-  file { '/var/lib/zuul/www/index.html':
-    ensure  => link,
-    target  => '/opt/zuul/etc/status/public_html/index.html',
-    require => File['/var/lib/zuul/www'],
-  }
-
-  file { '/var/lib/zuul/www/styles':
-    ensure  => link,
-    target  => '/opt/zuul/etc/status/public_html/styles',
-    require => File['/var/lib/zuul/www'],
-  }
-
-  file { '/var/lib/zuul/www/zuul.app.js':
-    ensure  => link,
-    target  => '/opt/zuul/etc/status/public_html/zuul.app.js',
-    require => File['/var/lib/zuul/www'],
-  }
-
-  file { '/var/lib/zuul/www/jquery.zuul.js':
-    ensure  => link,
-    target  => '/opt/zuul/etc/status/public_html/jquery.zuul.js',
-    require => File['/var/lib/zuul/www'],
-  }
-
-  file { '/var/lib/zuul/www/images':
-    ensure  => link,
-    target  => '/opt/zuul/etc/status/public_html/images',
-    require => File['/var/lib/zuul/www'],
-  }
-
   file { '/etc/init.d/zuul':
     ensure => present,
     owner  => 'root',
@@ -410,91 +297,14 @@ class zuul (
     source => 'puppet:///modules/zuul/zuul-executor.init',
   }
 
-  if $proxy_ssl_cert_file_contents == '' {
-    $ssl = false
-  } else {
-    $ssl = true
-    file { '/etc/ssl/certs':
-      ensure => directory,
-      owner  => 'root',
-      group  => 'root',
-      mode   => '0755',
-    }
-    file { '/etc/ssl/private':
-      ensure => directory,
-      owner  => 'root',
-      group  => 'root',
-      mode   => '0700',
-    }
-    file { "/etc/ssl/certs/${vhost_name}.pem":
-      ensure  => present,
-      owner   => 'root',
-      group   => 'root',
-      mode    => '0644',
-      content => $proxy_ssl_cert_file_contents,
-      require => File['/etc/ssl/certs'],
-      before  => Httpd::Vhost[$vhost_name],
-    }
-    file { "/etc/ssl/private/${vhost_name}.key":
-      ensure  => present,
-      owner   => 'root',
-      group   => 'root',
-      mode    => '0600',
-      content => $proxy_ssl_key_file_contents,
-      require => File['/etc/ssl/private'],
-      before  => Httpd::Vhost[$vhost_name],
-    }
-    if $proxy_ssl_chain_file_contents != '' {
-      file { "/etc/ssl/certs/${vhost_name}_intermediate.pem":
-        ensure  => present,
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0644',
-        content => $proxy_ssl_chain_file_contents,
-        require => File['/etc/ssl/certs'],
-        before  => Httpd::Vhost[$vhost_name],
-      }
+  if $webui {
+    class { '::zuul::webui':
+      vhost                         => $vhost_name,
+      serveradmin                   => $serveradmin,
+      proxy_ssl_cert_file_contents  => $proxy_ssl_cert_file_contents,
+      proxy_ssl_key_file_contents   => $proxy_ssl_key_file_contents,
+      proxy_ssl_chain_file_contents => $proxy_ssl_chain_file_contents,
+      block_referers                => $block_referers,
     }
   }
-
-  ::httpd::vhost { $vhost_name:
-    port       => 443, # Is required despite not being used.
-    docroot    => 'MEANINGLESS ARGUMENT',
-    priority   => '50',
-    ssl        => $ssl,
-    template   => 'zuul/zuul.vhost.erb',
-    vhost_name => $vhost_name,
-  }
-  if ! defined(Httpd::Mod['rewrite']) {
-    httpd::mod { 'rewrite': ensure => present }
-  }
-  if ! defined(Httpd::Mod['proxy']) {
-    httpd::mod { 'proxy': ensure => present }
-  }
-  if ! defined(Httpd::Mod['proxy_http']) {
-    httpd::mod { 'proxy_http': ensure => present }
-  }
-  if ! defined(Httpd::Mod['cache']) {
-    httpd::mod { 'cache': ensure => present }
-  }
-  if ! defined(Httpd::Mod['cgid']) {
-    httpd::mod { 'cgid': ensure => present }
-  }
-
-  case $::lsbdistcodename {
-    'precise': {
-      if ! defined(Httpd::Mod['mem_cache']) {
-        httpd::mod { 'mem_cache': ensure => present }
-      }
-      if ! defined(Httpd::Mod['version']) {
-        httpd::mod { 'version': ensure => present }
-      }
-    }
-    default: {
-      if ! defined(Httpd::Mod['cache_disk']) {
-        httpd::mod { 'cache_disk': ensure => present }
-      }
-    }
-  }
-
 }
