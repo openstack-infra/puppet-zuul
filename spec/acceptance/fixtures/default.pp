15a622c31030608e913ecf7df92f812ab5fa1b20
James E. Blair <jeblair@redhat.com>
2018-01-19 13:14:10 -0800

    Update spec acceptance to zuulv3
    Change-Id: I53e30b053e6963e85247769ec6443439f9144582

--- spec/acceptance/fixtures/default.pp
+++ spec/acceptance/fixtures/default.pp
@@ -2,14 +2,19 @@ class { '::zuul':
   proxy_ssl_cert_file_contents => file('/etc/ssl/certs/ssl-cert-snakeoil.pem'),
   proxy_ssl_key_file_contents  => file('/etc/ssl/private/ssl-cert-snakeoil.key'),
   zuul_ssh_private_key         => file('/tmp/zuul-ssh-keys/ssh_rsa_key'),
+  zuulv3                       => true,
 }
 
-class { '::zuul::server':
-  layout_dir => '/etc/project-config/zuul'
+class { '::zuul::scheduler':
+  layout_dir     => '/etc/project-config/zuul',
+  python_version => 3,
 }
 
 class { '::zuul::merger': }
-class { '::zuul::launcher': }
+class { '::zuul::executor': }
+class { '::zuul::web': }
+class { '::zuul::fingergw': }
+
 class { '::zuul::known_hosts':
   known_hosts_content => 'known_hosts_content',
 }
