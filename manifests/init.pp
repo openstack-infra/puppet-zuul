5fb79c2d14f7d8879112ae5e755d8db4c0d935e1
Monty Taylor <mordred@inaugust.com>
2018-03-02 19:37:28 -0500

    Update zuul-web install for javascript stack
    With the update to the use of yarn/webpack for the zuul-web javascript
    stack, there is now a pre-built tarball of the web assets. Update the
    code to use it.
    
    An alternative to this approach would be to install nodejs and yarn on
    the machine and just do a pip install . in the zuul source dir which
    would also work ... but since there is an option that doesn't require
    the need to run the build tools, why not use it?
    
    Depends-On: https://review.openstack.org/547790
    Change-Id: Ib88b48ddea26ef5d8d0d152774112844b180f77e

--- manifests/init.pp
+++ manifests/init.pp
@@ -114,8 +114,6 @@ class zuul (
   $packages = [
     'libffi-dev',
     'libssl-dev',
-    'python-paste',
-    'python-webob',
   ]
 
   package { $packages:
@@ -135,37 +133,39 @@ class zuul (
     require  => Class['pip'],
   }
 
-  # needed by python-keystoneclient, has system bindings
-  # Zuul and Nodepool both need it, so make it conditional
-  if ! defined(Package['python-lxml']) {
-    package { 'python-lxml':
-      ensure => present,
+  if ! $zuulv3 {
+    # needed by python-keystoneclient, has system bindings
+    # Zuul and Nodepool both need it, so make it conditional
+    if ! defined(Package['python-lxml']) {
+      package { 'python-lxml':
+        ensure => present,
+      }
     }
-  }
 
-  # A lot of things need yaml, be conservative requiring this package to avoid
-  # conflicts with other modules.
-  if ! defined(Package['python-yaml']) {
-    package { 'python-yaml':
-      ensure => present,
+    # A lot of things need yaml, be conservative requiring this package to avoid
+    # conflicts with other modules.
+    if ! defined(Package['python-yaml']) {
+      package { 'python-yaml':
+        ensure => present,
+      }
     }
-  }
 
-  if ! defined(Package['python-paramiko']) {
-    package { 'python-paramiko':
-      ensure   => present,
+    if ! defined(Package['python-paramiko']) {
+      package { 'python-paramiko':
+        ensure   => present,
+      }
     }
-  }
 
-  if ! defined(Package['python-daemon']) {
-    package { 'python-daemon':
-      ensure => present,
+    if ! defined(Package['python-daemon']) {
+      package { 'python-daemon':
+        ensure => present,
+      }
     }
-  }
 
-  if ! defined(Package['yui-compressor']) {
-    package { 'yui-compressor':
-      ensure => present,
+    if ! defined(Package['yui-compressor']) {
+      package { 'yui-compressor':
+        ensure => present,
+      }
     }
   }
 
@@ -199,15 +199,17 @@ class zuul (
       Package['build-essential'],
       Package['libffi-dev'],
       Package['libssl-dev'],
-      Package['python-daemon'],
-      Package['python-lxml'],
-      Package['python-paramiko'],
-      Package['python-paste'],
-      Package['python-webob'],
-      Package['python-yaml'],
       Package['yappi'],
-      Package['yui-compressor'],
     ],
+  }
+
+  if ! $zuulv3 {
+    Exec['install_zuul'] -> Package['python-daemon']
+    Exec['install_zuul'] -> Package['python-daemon']
+    Exec['install_zuul'] -> Package['python-lxml']
+    Exec['install_zuul'] -> Package['python-paramiko']
+    Exec['install_zuul'] -> Package['python-yaml']
+    Exec['install_zuul'] -> Package['yui-compressor']
   }
 
   file { '/etc/zuul':
@@ -365,117 +367,135 @@ class zuul (
     content => $zuul_ssh_private_key,
   }
 
-  file { '/var/lib/zuul/www':
-    ensure  => directory,
-    require => File['/var/lib/zuul'],
-  }
+  if $zuulv3 {
+    package { 'libjs-jquery':
+      ensure => absent,
+    }
+    $v2_web_dirs = [
+      '/opt/twitter-bootstrap',
+      '/opt/jquery-visibility',
+      '/var/lib/zuul/www',
+      '/opt/graphitejs',
+    ]
 
-  file { '/var/lib/zuul/www/lib':
-    ensure  => directory,
-    require => File['/var/lib/zuul/www'],
-  }
+    file { $v2_web_dirs:
+      ensure => absent,
+    }
 
-  package { 'libjs-jquery':
-    ensure => present,
-  }
+  } else {
 
-  file { '/var/lib/zuul/www/jquery.min.js':
-    ensure => absent
-  }
+    file { '/var/lib/zuul/www':
+      ensure  => directory,
+      require => File['/var/lib/zuul'],
+    }
 
-  file { '/var/lib/zuul/www/lib/jquery.min.js':
-    ensure  => link,
-    target  => '/usr/share/javascript/jquery/jquery.min.js',
-    require => [File['/var/lib/zuul/www/lib'],
-                Package['libjs-jquery']],
-  }
+    file { '/var/lib/zuul/www/lib':
+      ensure  => directory,
+      require => File['/var/lib/zuul/www'],
+    }
 
-  vcsrepo { '/opt/twitter-bootstrap':
-    ensure   => latest,
-    provider => git,
-    revision => 'v3.1.1',
-    source   => 'https://github.com/twbs/bootstrap.git',
-  }
+    package { 'libjs-jquery':
+      ensure => present,
+    }
 
-  file { '/var/lib/zuul/www/bootstrap':
-    ensure => absent
-  }
+    file { '/var/lib/zuul/www/jquery.min.js':
+      ensure => absent
+    }
 
-  file { '/var/lib/zuul/www/lib/bootstrap':
-    ensure  => link,
-    target  => '/opt/twitter-bootstrap/dist',
-    require => [File['/var/lib/zuul/www/lib'],
-                Package['libjs-jquery'],
-                Vcsrepo['/opt/twitter-bootstrap']],
-  }
+    file { '/var/lib/zuul/www/lib/jquery.min.js':
+      ensure  => link,
+      target  => '/usr/share/javascript/jquery/jquery.min.js',
+      require => [File['/var/lib/zuul/www/lib'],
+                  Package['libjs-jquery']],
+    }
 
-  vcsrepo { '/opt/jquery-visibility':
-    ensure   => latest,
-    provider => git,
-    revision => 'master',
-    source   => 'https://github.com/mathiasbynens/jquery-visibility.git',
-  }
+    vcsrepo { '/opt/twitter-bootstrap':
+      ensure   => latest,
+      provider => git,
+      revision => 'v3.1.1',
+      source   => 'https://github.com/twbs/bootstrap.git',
+    }
 
-  file { '/var/lib/zuul/www/jquery-visibility.min.js':
-    ensure => absent
-  }
+    file { '/var/lib/zuul/www/bootstrap':
+      ensure => absent
+    }
 
-  exec { 'install-jquery-visibility':
-    command     => 'yui-compressor -o /var/lib/zuul/www/lib/jquery-visibility.js /opt/jquery-visibility/jquery-visibility.js',
-    path        => 'bin:/usr/bin',
-    refreshonly => true,
-    subscribe   => Vcsrepo['/opt/jquery-visibility'],
-    require     => [File['/var/lib/zuul/www/lib'],
-                    Package['yui-compressor'],
-                    Vcsrepo['/opt/jquery-visibility']],
-  }
+    file { '/var/lib/zuul/www/lib/bootstrap':
+      ensure  => link,
+      target  => '/opt/twitter-bootstrap/dist',
+      require => [File['/var/lib/zuul/www/lib'],
+                  Package['libjs-jquery'],
+                  Vcsrepo['/opt/twitter-bootstrap']],
+    }
 
-  vcsrepo { '/opt/graphitejs':
-    ensure   => latest,
-    provider => git,
-    revision => 'master',
-    source   => 'https://github.com/prestontimmons/graphitejs.git',
-  }
+    vcsrepo { '/opt/jquery-visibility':
+      ensure   => latest,
+      provider => git,
+      revision => 'master',
+      source   => 'https://github.com/mathiasbynens/jquery-visibility.git',
+    }
 
-  file { '/var/lib/zuul/www/jquery.graphite.js':
-    ensure => absent
-  }
+    file { '/var/lib/zuul/www/jquery-visibility.min.js':
+      ensure => absent
+    }
 
-  file { '/var/lib/zuul/www/lib/jquery.graphite.js':
-    ensure  => link,
-    target  => '/opt/graphitejs/jquery.graphite.js',
-    require => [File['/var/lib/zuul/www/lib'],
-                Vcsrepo['/opt/graphitejs']],
-  }
+    exec { 'install-jquery-visibility':
+      command     => 'yui-compressor -o /var/lib/zuul/www/lib/jquery-visibility.js /opt/jquery-visibility/jquery-visibility.js',
+      path        => 'bin:/usr/bin',
+      refreshonly => true,
+      subscribe   => Vcsrepo['/opt/jquery-visibility'],
+      require     => [File['/var/lib/zuul/www/lib'],
+                      Package['yui-compressor'],
+                      Vcsrepo['/opt/jquery-visibility']],
+    }
 
-  file { '/var/lib/zuul/www/index.html':
-    ensure  => link,
-    target  => '/opt/zuul/etc/status/public_html/index.html',
-    require => File['/var/lib/zuul/www'],
-  }
+    vcsrepo { '/opt/graphitejs':
+      ensure   => latest,
+      provider => git,
+      revision => 'master',
+      source   => 'https://github.com/prestontimmons/graphitejs.git',
+    }
 
-  file { '/var/lib/zuul/www/styles':
-    ensure  => link,
-    target  => '/opt/zuul/etc/status/public_html/styles',
-    require => File['/var/lib/zuul/www'],
-  }
+    file { '/var/lib/zuul/www/jquery.graphite.js':
+      ensure => absent
+    }
 
-  file { '/var/lib/zuul/www/zuul.app.js':
-    ensure  => link,
-    target  => '/opt/zuul/etc/status/public_html/zuul.app.js',
-    require => File['/var/lib/zuul/www'],
-  }
+    file { '/var/lib/zuul/www/lib/jquery.graphite.js':
+      ensure  => link,
+      target  => '/opt/graphitejs/jquery.graphite.js',
+      require => [File['/var/lib/zuul/www/lib'],
+                  Vcsrepo['/opt/graphitejs']],
+    }
 
-  file { '/var/lib/zuul/www/jquery.zuul.js':
-    ensure  => link,
-    target  => '/opt/zuul/etc/status/public_html/jquery.zuul.js',
-    require => File['/var/lib/zuul/www'],
-  }
+    file { '/var/lib/zuul/www/index.html':
+      ensure  => link,
+      target  => '/opt/zuul/etc/status/public_html/index.html',
+      require => File['/var/lib/zuul/www'],
+    }
 
-  file { '/var/lib/zuul/www/images':
-    ensure  => link,
-    target  => '/opt/zuul/etc/status/public_html/images',
-    require => File['/var/lib/zuul/www'],
+    file { '/var/lib/zuul/www/styles':
+      ensure  => link,
+      target  => '/opt/zuul/etc/status/public_html/styles',
+      require => File['/var/lib/zuul/www'],
+    }
+
+    file { '/var/lib/zuul/www/zuul.app.js':
+      ensure  => link,
+      target  => '/opt/zuul/etc/status/public_html/zuul.app.js',
+      require => File['/var/lib/zuul/www'],
+    }
+
+    file { '/var/lib/zuul/www/jquery.zuul.js':
+      ensure  => link,
+      target  => '/opt/zuul/etc/status/public_html/jquery.zuul.js',
+      require => File['/var/lib/zuul/www'],
+    }
+
+    file { '/var/lib/zuul/www/images':
+      ensure  => link,
+      target  => '/opt/zuul/etc/status/public_html/images',
+      require => File['/var/lib/zuul/www'],
+    }
   }
 
   if $zuulv3 {
